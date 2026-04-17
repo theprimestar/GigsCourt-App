1d31ef5adf93027ba735a5a1c5d74a1886826cc0
Gigscourt <theprimestarventures@gmail.com>
2026-04-17 21:12:27 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// ========================================
+// GigsCourt - Firebase Configuration
+// ========================================
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAqvDHUPuGtZGMephb3dN_31eruuBXnbFE",
+    authDomain: "gigscourt2.firebaseapp.com",
+    projectId: "gigscourt2",
+    storageBucket: "gigscourt2.firebasestorage.app",
+    messagingSenderId: "505136313803",
+    appId: "1:505136313803:web:2b61e6916efdaf8723324e"
+};
+
+// Also make available as default export for flexibility
+export default firebaseConfig;
